9a03c9ceeaa85fc04f0fc65a6f75a3c015e993e9
Crab <crabcodes28@gmail.com>
2023-05-23 19:42:09 +0200

    Add logic to select game results

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ function resetGame() {
     });
 
     computerWindow.textContent = 'Pick your move!';
-
 }
 
 function createListeners() {
@@ -77,6 +76,11 @@ function startRound(cSelection, pSelection) {
 
         if (windowInterval >= 4) {
             clearInterval(gameInterval);
+            displayComputerIcon(cSelection);
+
+            setTimeout(() => {
+                postGameDisplay(cSelection, pSelection);
+            }, 1200);
         }
     }, 800);
 }
@@ -88,7 +92,49 @@ function disableButtons() {
     });
 }
 
-let playerOption = null;
+function displayComputerIcon(cSelection) {
+    computerWindow.textContent = '';
+    computerWindow.classList.add('more-window-font');
+    computerWindow.classList.add('fa-regular');
+    if (cSelection === 1) {
+        computerWindow.classList.add('fa-hand-back-fist');
+    }
+    else if (cSelection === 2) {
+        computerWindow.classList.add('fa-hand-peace');
+    }
+    else {
+        computerWindow.classList.add('fa-hand');
+    }
+}
+
+function selectWinner(cSelection, pSelection) {
+    if (cSelection === pSelection) {
+        return "It's a Draw!";
+    }
+    else if (
+        cSelection === 1 && pSelection === 2 || 
+        cSelection === 2 && pSelection === 3 ||
+        cSelection === 3 && pSelection === 1) {
+        return 'Computer Wins!';
+    }
+    else {
+        return 'Player Wins!';
+    }
+}
+
+function postGameDisplay(cSelection, pSelection) {
+    computerWindow.classList.remove('more-window-font');
+    computerWindow.classList.remove('fa-regular');
+    computerWindow.classList.remove('fa-hand-back-fist');
+    computerWindow.classList.remove('fa-hand-peace');
+    computerWindow.classList.remove('fa-hand');
+
+    computerWindow.textContent = selectWinner(cSelection, pSelection);
+
+    setTimeout(() => {
+        resetGame();
+    }, 1000);
+}
 
 resetGame();
 createListeners();
